90cde4c52e2b38fe9cbe27053ab5d0d9fe03cf8a
amine-zzr <aminezoukri@gmail.com>
2024-01-19 21:22:00 +0000

    the code is modified to iterate over each character in the token to verify that it is a digit or, in the case of the first character, a potential negative sign

--- parser.c
+++ parser.c
@@ -11,7 +11,7 @@ void parser(char *line, unsigned int line_number)
 {
 	char *token;
 	char opcode[256];
-	int arg;
+	int arg, i;
 
 	token = strtok(line, " \t\n");
 	if (token == NULL)
@@ -27,19 +27,18 @@ void parser(char *line, unsigned int line_number)
 			fprintf(stderr, "L%u: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
+		for (i = 0; token[i] != '\0'; i++)
+		{
+			if (!isdigit(token[i]) && !(i == 0 && token[i] == '-'))
+			{
+				fprintf(stderr, "L%u: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+		}
 
 		arg = atoi(token);
+		push(&stack, arg);
 
-		while (isspace((unsigned char)*token))
-			token++;
-
-		if (isdigit(*token) || (*token == '-' && isdigit(*(token + 1))))
-			push(&stack, arg);
-		else
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
 	}
 	else if (strcmp(opcode, "pall") == 0)
 		pall(&stack);
