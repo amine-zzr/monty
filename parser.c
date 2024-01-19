c69cdbddfd066a200ea75b668c29e9438161950e
amine-zzr <aminezoukri@gmail.com>
2024-01-19 20:46:27 +0000

    trying different approach for parsing

--- parser.c
+++ parser.c
@@ -9,44 +9,43 @@
 
 void parser(char *line, unsigned int line_number)
 {
+	char *token;
 	char opcode[256];
-	char *tmp_ptr;
 	int arg;
 
-	if (sscanf(line, "%255s", opcode) == 1)
+	token = strtok(line, " \t\n");
+	if (token == NULL)
+		return;
+
+	strcpy(opcode, token);
+
+	if (strcmp(opcode, "push") == 0)
 	{
-		if (strcmp(opcode, "push") == 0)
+		token = strtok(NULL, " \t\n");
+		if (token == NULL)
 		{
-			if (sscanf(line + strlen("push"), "%d", &arg) == 1)
-			{
-				tmp_ptr = line + strlen("push");
-				while (*tmp_ptr && (*tmp_ptr == ' ' || *tmp_ptr == '\t'))
-					tmp_ptr++;
-
-				if (isdigit(*tmp_ptr) || (*tmp_ptr == '-' && isdigit(*(tmp_ptr + 1))))
-					push(&stack, arg);
-				else
-				{
-					fprintf(stderr, "L%u, usage: push integer\n", line_number);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				fprintf(stderr, "L%u, usage: push integer\n", line_number);
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		else if (strcmp(opcode, "pall") == 0)
-			pall(&stack);
-		else
-		{
-			while (*line && (*line != '\n' && *line != ' ' && *line != '\t'))
-				line++;
-
-			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
+
+		arg = atoi(token);
+
+		while (isspace((unsigned char)*token))
+			token++;
+
+		if (isdigit(*token) || (*token == '-' && isdigit(*(token + 1))))
+			push(&stack, arg);
+		else
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (strcmp(opcode, "pall") == 0)
+		pall(&stack);
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
 	}
 }
