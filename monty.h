be826f066dd67f1f1884b393c602993a63253487
amine-zzr <aminezoukri@gmail.com>
2024-01-20 01:01:10 +0000

    functions signatures modification to handle new parsing methode

--- monty.h
+++ monty.h
@@ -43,9 +43,11 @@ extern stack_t *stack;
 
 
 void parser(char *line, unsigned int line_number);
-void push(stack_t **stack, int value);
-void pall(stack_t **stack);
+void push(char *token, unsigned int line_number);
+void pall(void);
 void free_stack(void);
+void pop(unsigned int line_number);
+void pint(unsigned int line_number);
 
 
 #endif /* MONTY_H */
