c906c09295ed75f340960770da1e8a0aa9792eab
amine-zzr <aminezoukri@gmail.com>
2024-01-20 01:03:28 +0000

    the monty interpreter now handles the pop and pint opcodes

---
+++ opcodes1.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * pint - Handles the pint opcode in Monty language
+ * @line_number: The line number in the Monty bytecode file
+ *
+ */
+
+void pint(unsigned int line_number)
+{
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", stack->n);
+}
+
+/**
+ * pop - Handles the pop opcode in Monty language
+ * @line_number: The line number in the Monty bytecode file
+ *
+ */
+
+void pop(unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = stack;
+	stack = stack->next;
+	stack->prev = NULL;
+	free(temp);
+}
