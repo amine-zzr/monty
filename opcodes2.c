f21a1329586f87769d4739afcd42a1deb2d24539
amine-zzr <aminezoukri@gmail.com>
2024-01-21 16:33:41 +0000

    mod opcode supported

--- opcodes2.c
+++ opcodes2.c
@@ -60,3 +60,29 @@ void mul(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n *= (*stack)->n;
 	pop(stack, line_number);
 }
+
+
+/**
+ * mod - Computes the rest of the division of the second
+ *	top element by the top element.
+ * @stack: The pointer to the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
+	pop(stack, line_number);
+}
