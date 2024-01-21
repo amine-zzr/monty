6501bb75fe7180f0d8f108ad3f5f88f7e27016cd
amine-zzr <aminezoukri@gmail.com>
2024-01-21 17:11:52 +0000

    printing the whole stack elements as a string is now supported

--- opcodes3.c
+++ opcodes3.c
@@ -23,3 +23,32 @@ void pchar(stack_t **stack, unsigned int line_number)
 	putchar((*stack)->n);
 	putchar('\n');
 }
+
+
+/**
+ * pstr - Prints the string starting at the top of the stack.
+ * @stack: The pointer to the stack.
+ * @line_number: The line number in the Monty byte code file.
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	(void)line_number;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		putchar('\n');
+		return;
+	}
+
+	current = *stack;
+	while (current && current->n != 0 && current->n > 0 && current->n <= 127)
+	{
+		putchar(current->n);
+		current = current->next;
+	}
+
+	putchar('\n');
+}
